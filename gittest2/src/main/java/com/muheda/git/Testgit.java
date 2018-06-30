fc26e9d750a20452de79a6574bdd76ab53a2a8a5
cpy <18713019005@163.com>
2018-06-30 14:33:20 +0800

    修改为斯诺克

--- gittest2/src/main/java/com/muheda/git/Testgit.java
+++ gittest2/src/main/java/com/muheda/git/Testgit.java
@@ -1,8 +1,7 @@
 package com.muheda.git;
 
 public class Testgit {
-	public static void main(String[] args) {
-		System.out.println("我是一个勤,并且能坚持,爱动脑的人");
-	}
-
+  public static void main(String[] args) {
+	System.out.println("斯诺克");
+}
 }
